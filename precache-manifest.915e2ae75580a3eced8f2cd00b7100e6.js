bb841b91d5023241f0d5467c1882e769ea39a7ee
Mehul2112 <72200573+Mehul2112@users.noreply.github.com>
2021-01-15 13:20:10 +0530

    Add files via upload

---
+++ precache-manifest.915e2ae75580a3eced8f2cd00b7100e6.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "00f812a1aa123bcb8ba6",
+    "url": "/static/css/main.fb87b131.chunk.css"
+  },
+  {
+    "revision": "00f812a1aa123bcb8ba6",
+    "url": "/static/js/main.00f812a1.chunk.js"
+  },
+  {
+    "revision": "df7eaa8eb0cce4cfb73a",
+    "url": "/static/js/1.df7eaa8e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e13e79b49fb98a2b3764d227b9231ae7",
+    "url": "/static/media/kick-tape.e13e79b4.wav"
+  },
+  {
+    "revision": "82a6ad655443a87b3cd647c857a78cfa",
+    "url": "/static/media/clap-tape.82a6ad65.wav"
+  },
+  {
+    "revision": "46ba2a481c9caaaf9c8acfd65e4626e5",
+    "url": "/static/media/hihat-808.46ba2a48.wav"
+  },
+  {
+    "revision": "9edd60fedb45e42f1cbce056c7a1c493",
+    "url": "/static/media/C4.9edd60fe.mp3"
+  },
+  {
+    "revision": "b7fca1830eb61b579a2acb3f07ef5476",
+    "url": "/static/media/play.b7fca183.svg"
+  },
+  {
+    "revision": "1f2bf351ec4ef8b1d78623f9c32b7b38",
+    "url": "/static/media/stop.1f2bf351.svg"
+  },
+  {
+    "revision": "550be808a1e25f5b4db6b66350270316",
+    "url": "/static/media/kick-808.550be808.wav"
+  },
+  {
+    "revision": "6f0a525e4a9b0b51eb5c9fd5954229de",
+    "url": "/static/media/kick-electro01.6f0a525e.wav"
+  },
+  {
+    "revision": "071632682e36246e277822c52d4c65ff",
+    "url": "/static/media/kick-classic.07163268.wav"
+  },
+  {
+    "revision": "98b1a6e1c2de613f00ab3d16b12c316c",
+    "url": "/static/media/clap-fat.98b1a6e1.wav"
+  },
+  {
+    "revision": "5c87ec3f1a84ae4b9c7404347a1b2ab8",
+    "url": "/static/media/clap-crushed.5c87ec3f.wav"
+  },
+  {
+    "revision": "39f9581affa38e7f9322157d85e1ff57",
+    "url": "/static/media/clap-808.39f9581a.wav"
+  },
+  {
+    "revision": "f0989e249981ccd3de464be76301c6b0",
+    "url": "/static/media/hihat-acoustic01.f0989e24.wav"
+  },
+  {
+    "revision": "97009a17812c7eaed850b564ebab7435",
+    "url": "/static/media/hihat-electro.97009a17.wav"
+  },
+  {
+    "revision": "0dff397fdb74900cbbe29def6f71e90d",
+    "url": "/static/media/C4.0dff397f.mp3"
+  },
+  {
+    "revision": "b253a180cfcd431fa013574016d2b2b5",
+    "url": "/static/media/C4.b253a180.mp3"
+  },
+  {
+    "revision": "749d66a6b947daf5774edc84723ac843",
+    "url": "/index.html"
+  }
+];
